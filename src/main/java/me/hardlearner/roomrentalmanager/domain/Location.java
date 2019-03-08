fe1a825847d00a54a5625fa2a031f0d96e2e20e1
David <xodnjs2549@naver.com>
2019-03-08 16:38:41 +0900

    edit piano category mapping type in location

--- src/main/java/me/hardlearner/roomrentalmanager/domain/Location.java
+++ src/main/java/me/hardlearner/roomrentalmanager/domain/Location.java
@@ -1,9 +1,6 @@
 package me.hardlearner.roomrentalmanager.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Location {
@@ -12,10 +9,11 @@ public class Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private int roomNo;
-    private Enum<PianoCategory> pianoCategory;
+    @Enumerated(EnumType.STRING)
+    private PianoCategory pianoCategory;
     private int pianoCount;
 
-    public Location(int roomNo, Enum pianoCategory, int pianoCount) {
+    public Location(int roomNo, PianoCategory pianoCategory, int pianoCount) {
         this.roomNo = roomNo;
         this.pianoCategory = pianoCategory;
         this.pianoCount = pianoCount;
@@ -39,10 +37,6 @@ public class Location {
 
     public Enum getPianoCategory() {
         return pianoCategory;
-    }
-
-    public void setPianoCategory(Enum pianoCategory) {
-        this.pianoCategory = pianoCategory;
     }
 
     public int getPianoCount() {
